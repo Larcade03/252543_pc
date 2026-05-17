3974b2ec1cdfeee8977332ed8db70cf678bcd3d2
Larcade03 <kevelev1589@gmail.com>
2026-05-16 20:42:03 -0500

    Parte 5

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -1,61 +1,68 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.8.2 <0.9.0;
 
 import "hardhat/console.sol";
 
-contract Almacen252543{
-    ///252543
+contract Almacen252543 {
+
     struct Producto {
         uint256 id;
         string nombre;
         uint256 stock;
-        bool estado;
+        bool estado; 
     }
- 
-     Producto[] public productos;
-     address public dirContrato;
+
+    mapping(uint256 => Producto) public productos;
+
+    uint256[] private historialIds;
+
+    uint256 public cantidad;
+    
+    address public dirContrato;
 
     modifier registrarLog() {
         console.log("Ejecutado por: 252543 - Bray Luigui Vargas Perez");
         _;
     }
+
     constructor() registrarLog {
         dirContrato = address(this);
     }
 
-   function agregarElemento(uint256 _id, string memory _nombre, uint256 _stock) public registrarLog {
-        
-        for (uint256 i = 0; i < productos.length; i++) {
-            require(productos[i].id != _id, "Producto con ese ID ya existe");
-        }
-
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _stock) public registrarLog {
+        require(productos[_id].id == 0, "Producto con ese ID ya existe");
         require(_stock > 0, "El stock debe ser mayor a cero");
 
-        productos.push(Producto(_id, _nombre, _stock, true));
+        productos[_id] = Producto(_id, _nombre, _stock, true);
+        
+        historialIds.push(_id);
+
+        cantidad++;
     }
 
     function contarElementos() public view registrarLog returns (uint256) {
-        return productos.length;
+        return cantidad;
     }
 
-    function inactivarElemento(uint256 _posicion) public registrarLog {
-        require(_posicion < productos.length, "Posicion fuera de rango");
-        productos[_posicion].estado = false;
+    function inactivarElemento(uint256 _id) public registrarLog {
+        require(productos[_id].id == _id, "El producto no existe");
+        productos[_id].estado = false;
     }
 
     function pintarElementosActivos() public view registrarLog {
-        for (uint256 i = 0; i < productos.length; i++) {
-            if (productos[i].estado == true) {
-                console.log("Producto activo:", productos[i].id, productos[i].nombre);
+        for (uint256 i = 0; i < historialIds.length; i++) {
+            uint256 currentId = historialIds[i];
+            if (productos[currentId].estado == true) {
+                console.log("Producto activo:", productos[currentId].id, productos[currentId].nombre);
             }
         }
     }
 
     function pintarElementosImpares() public view registrarLog {
-        for (uint256 i = 0; i < productos.length; i++) {
-            if (productos[i].id % 2 != 0) {
-                console.log("Producto ID impar:", productos[i].id, productos[i].nombre);
+        for (uint256 i = 0; i < historialIds.length; i++) {
+            uint256 currentId = historialIds[i];
+            if (productos[currentId].id % 2 != 0) {
+                console.log("Producto ID impar:", productos[currentId].id, productos[currentId].nombre);
             }
         }
     }
