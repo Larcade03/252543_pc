dda946e50b9862e0be642b751cde92b65af1c6ef
Larcade03 <kevelev1589@gmail.com>
2026-05-16 20:17:28 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -16,5 +16,16 @@ contract Almacen252543{
 
     constructor() {
         console.log("Ejecutado por: 252543- Bray Luigui Vargas Perez");
+
+        //salida: contract address:  0xDA0bab807633f07f013f94DD0E6A4F96F8742B53
+    }
+
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _stock) public {
+        productos.push(Producto(_id, _nombre, _stock));
+    }
+
+    function contarElementos() public view returns (uint256) {
+        console.log("Ejecutado por: 252543 - Bray Luigui Vargas Perez");
+        return productos.length;
     }
 }
